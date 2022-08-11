b26de2b1c9a421238c8c220c8e0f5f6ce14dca01
nellfs <pokycelit@gmail.com>
2022-08-11 19:18:15 -0300

    styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,14 @@
+import { ThemeProvider } from "styled-components";
+import theme from "./styles/theme";
+import GlobalStyle from "./styles/globalStyle";
+
 function App() {
-  return <div className="App">hello</div>;
+  return (
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+      <div className="App">hello</div>
+    </ThemeProvider>
+  );
 }
 
 export default App;
---
+++ src/pages/Home/Home.tsx
@@ -0,0 +1,5 @@
+const Home = () => {
+  return <div>nothing here</div>;
+};
+
+export default Home;
